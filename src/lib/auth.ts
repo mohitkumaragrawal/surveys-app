a434c1de95721c6ef639b250570682197baa32a2
Mohit Kumar Agrawal <mhtkrag@gmail.com>
2023-07-01 17:49:08 +0530

    Initialized prisma and added PrismaAdapter for NextAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,15 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
+
+const prisma = new PrismaClient();
+
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+
   session: {
     strategy: "jwt",
   },
